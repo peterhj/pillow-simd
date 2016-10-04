6e08ae1f1e37ddd2e52a444809434a714ff4b87d
homm <homm86@gmail.com>
2016-10-04 18:35:38 +0300

    package name

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ except (ImportError, OSError):
     # pypy emits an oserror
     _tkinter = None
 
-NAME = 'Pillow'
+NAME = 'Pillow-SIMD'
 PILLOW_VERSION = get_version()
 JPEG_ROOT = None
 JPEG2K_ROOT = None
